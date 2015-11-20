e3687701ae3447df8d27b491763b6127ac0e0520
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-11-20 03:12:12 +0100

    Using thread pool

--- src/main/java/org/gp/spyder/App.java
+++ src/main/java/org/gp/spyder/App.java
@@ -2,7 +2,10 @@ package org.gp.spyder;
 
 import java.io.File;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.gp.spyder.crawl.Crawler;
@@ -42,7 +45,7 @@ public class App {
     	recursiveDelete(file);
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
     	
 //    	deleteDatabase();
     	
@@ -65,16 +68,41 @@ public class App {
         	urlQueue.add(webPage.getUrl());
         }
         
-        Thread crawler = new Crawler(dbContext, urlQueue, dataQueue);
-        Thread parser = new Parser(dbContext, baseUrl, urlQueue, dataQueue, imgQueue);
-        Thread harvester = new Harvester(dbContext, DOWNLOAD_DIR, imgQueue);      
-        
         LOGGER.info(String.format("Starting with URL Queue of with %d entries", urlQueue.size()));
-        LOGGER.info("Starting Crawler, Parser and Harvester workers...");
+        LOGGER.info("Starting Crawler, Parser and Harvester workers...");        
         
-        crawler.start();
-        parser.start();
-        harvester.start();
+        int  corePoolSize  =    6;
+        int  maxPoolSize   =   12;
+        long keepAliveTime = 5000;
+
+        ExecutorService threadPoolExecutor =
+                new ThreadPoolExecutor(
+                        corePoolSize,
+                        maxPoolSize,
+                        keepAliveTime,
+                        TimeUnit.MILLISECONDS,
+                        new LinkedBlockingQueue<Runnable>()
+                        );
+        
+        threadPoolExecutor.submit(new Crawler(dbContext, urlQueue, dataQueue));
+        threadPoolExecutor.submit(new Crawler(dbContext, urlQueue, dataQueue));
+        threadPoolExecutor.submit(new Parser(dbContext, baseUrl, urlQueue, dataQueue, imgQueue));
+        threadPoolExecutor.submit(new Parser(dbContext, baseUrl, urlQueue, dataQueue, imgQueue));
+        threadPoolExecutor.submit(new Harvester(dbContext, DOWNLOAD_DIR, imgQueue));
+        threadPoolExecutor.submit(new Harvester(dbContext, DOWNLOAD_DIR, imgQueue));
+        
+        while(threadPoolExecutor.isTerminated() == false) {
+        	Thread.sleep(1000);
+        }
+        
+//        Thread crawler = new Crawler(dbContext, urlQueue, dataQueue);
+//        Thread parser = new Parser(dbContext, baseUrl, urlQueue, dataQueue, imgQueue);
+//        Thread harvester = new Harvester(dbContext, DOWNLOAD_DIR, imgQueue);      
+//        
+//        
+//        crawler.start();
+//        parser.start();
+//        harvester.start();
         
         //TODO: when to stop?
     }
